0e71b951a68cc1ed9d0b4ccbb283be235914c73a
Nafim <snrahman2010@gmail.com>
2021-02-07 14:09:48 -0500

    added menu support for city selection

--- src/views/Home/Intro/GameMenus/PrivateGame.tsx
+++ src/views/Home/Intro/GameMenus/PrivateGame.tsx
@@ -1,9 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { Link as RouterLink} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import {
     Button,
     CircularProgress,
+    Collapse,
+    FormControl,
     Link,
+    MenuItem,
+    Select,
     Typography,
 } from '@material-ui/core';
 import { getGameID } from '../../../../api/HTTPRequests';
@@ -13,13 +17,11 @@ import { useSnackbar } from 'notistack';
 import constants from '../../../../constants';
 
 const useStyles = makeStyles((theme) => ({
-    instructions: {
-        marginLeft: theme.spacing(2),
-        marginRight: theme.spacing(2),
-        marginTop: theme.spacing(1),
-        marginBottom: theme.spacing(2),
-        display: 'flex',
-        justifyContent: 'center'
+    root: {
+        padding: theme.spacing(0, 3, 0)
+    },
+    items: {
+        margin: theme.spacing(0, 0, 2),
     },
     cancelButton: {
         marginTop: theme.spacing(1),
@@ -28,7 +30,7 @@ const useStyles = makeStyles((theme) => ({
     centered: {
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'center'
+        flexDirection: 'column'
     }
 }));
 
@@ -42,58 +44,82 @@ function PrivateGame(props: PrivateGameProps) {
     const { enqueueSnackbar } = useSnackbar();
 
     const [gameID, setGameID] = useState("");
+    const [city, setCity] = useState("");
+    const [loading, setLoading] = useState(false);
 
     const handleCancel = () => {
         props.handleStateChange(MainMenuState.LandingMenu);
     }
 
-    const fetchGameID = () => {
-        getGameID()
-        .then(data => {
-            if (data.error) {
-                enqueueSnackbar(data.error, { 
-                    variant: 'error',
-                })
-            }
-            if (data.gameID) {
-                setGameID(data.gameID);
-            }
-        })
-        .catch(err => {
-            enqueueSnackbar(constants.ERROR_MESSAGE, { 
-                variant: 'error',
-            })
-        })
+    const handleCitySelect = (evt: React.ChangeEvent<{ value: unknown}>) => {
+        setCity(evt.target.value as string);
+        setLoading(true);
+        fetchGameID(evt.target.value as string);
     }
 
-    useEffect(() => {
-        fetchGameID();
-    }, [])
+    const fetchGameID = (city: string) => {
+        getGameID(city)
+            .then(data => {
+                if (data.error) {
+                    enqueueSnackbar(data.error, {
+                        variant: 'error',
+                    })
+                }
+                if (data.gameID) {
+                    setGameID(data.gameID);
+                }
+                setLoading(false);
+            })
+            .catch(err => {
+                enqueueSnackbar(constants.ERROR_MESSAGE, {
+                    variant: 'error',
+                })
+                setLoading(false);
+            })
+    }
 
     return (
-        <div>
-            <div className={classes.instructions}>
+        <div className={classes.root}>
+            <div className={classes.centered}>
                 <Typography variant='subtitle1' align='center' color='textSecondary'>
-                    Join the game using the following link:
+                    Select a city:
                 </Typography>
+                <FormControl className={classes.items}>
+                    {/* <InputLabel id="select-city">City</InputLabel> */}
+                    <Select
+                        labelId="select-city"
+                        inputProps={{min: 0, style: { textAlign: 'center' }}}
+                        id="select-city"
+                        value={city}
+                        defaultValue='city'
+                        onChange={handleCitySelect}
+                    >
+                        <MenuItem value={'Boston'}>Boston</MenuItem>
+                        <MenuItem value={'Buffalo'}>Buffalo</MenuItem>
+                        <MenuItem value={'New York'}>New York</MenuItem>
+                    </Select>
+                </FormControl>
             </div>
-            <div className={classes.instructions}>
-                {gameID && 
-                    <Typography variant='h5' align='center' color='textPrimary'>
-                            <Link component={RouterLink} to={`/play/${gameID}`}>
-                                {`${process.env.REACT_APP_WEBSITE_DOMAIN}/play/${gameID}`}
-                            </Link>
+            <div className={classes.centered}>
+                <Collapse in={!!gameID} unmountOnExit>
+                    <Typography variant='subtitle1' align='center' color='textSecondary'>
+                        Join the game using the following link:
                     </Typography>
-                }
-                {!gameID &&
+                    <Typography variant='h5' align='center' color='textPrimary'>
+                        <Link component={RouterLink} to={`/play/${gameID}`}>
+                            {`${process.env.REACT_APP_WEBSITE_DOMAIN}/play/${gameID}`}
+                        </Link>
+                    </Typography>
+                </Collapse>
+                <Collapse in={loading} unmountOnExit>
                     <CircularProgress />
-                }
+                </Collapse>
             </div>
             <div className={classes.centered}>
                 <Button
-                className={classes.cancelButton} 
-                color="secondary"
-                onClick={e => handleCancel()}
+                    className={classes.cancelButton}
+                    color="secondary"
+                    onClick={e => handleCancel()}
                 >
                     Cancel
                 </Button>
